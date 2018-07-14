7e77df2f2443bed716e48b09f99611debe7d9a93
Victor <tminhngvt1998@gmail.com>
2018-07-14 16:27:02 +1000

    Modify style.css and main.php

--- application/views/main.php
+++ application/views/main.php
@@ -1,8 +1,11 @@
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<img src="assets/img/Status_green.png" class="img-thumbnail rounded-circle">
+		<div class="col border border-primary task-label">
+			<p>Dusting Time</p>
+		</div>
+		<div class="col border border-primary time">
+			<p>Further Information</p>
+		</div>
 </div>
-<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>

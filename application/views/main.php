9e83f892502d9bc95c6d4d5e3321de873d3aac60
Victor <tminhngvt1998@gmail.com>
2018-07-14 17:17:06 +1000

    Modify style.css and main.php

--- application/views/main.php
+++ application/views/main.php
@@ -3,13 +3,83 @@
 	<div class="row">
 
 		<div class="col col-lg-1 no-gutter status">
-			<img src="assets/img/Status_green.png" class="status-light rounded-circle">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
 		</div>
-		<div class="col col-lg-2 border border-primary task-label">
-			<p>Dusting Time</p>
+		<div class="col border border-primary task-label">
+			<h2>Coal Production</h2>
 		</div>
-		<div class="col col-lg-2 border border-primary">
-			<p>Further Information</p>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Dusting Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Roof Grouting Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Spillage Cleanup Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Ventilation Tube Extenstion Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Service Extenstion Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
 		</div>
 
 	</div>

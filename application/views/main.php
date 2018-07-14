c59e887229b5e66ab8cb37221cb931f1b0b6ebb1
Victor <tminhngvt1998@gmail.com>
2018-07-14 18:03:23 +1000

    Update main.php

--- application/views/main.php
+++ application/views/main.php
@@ -6,26 +6,12 @@
 			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
 		</div>
 		<div class="col border border-primary task-label">
-			<h2>Coal Production</h2>
-		</div>
-		<div class="col border border-primary">
-			<h2>Numbers</h2>
-		</div>
-
-	</div>
-
-	<div class="row">
-
-		<div class="col col-lg-1 no-gutter status">
-			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
-		</div>
-		<div class="col border border-primary task-label">
 			<h2>Dusting Time</h2>
 		</div>
 		<div class="col border border-primary">
 			<h2>Numbers</h2>
 		</div>
-
+		
 	</div>
 
 	<div class="row">
@@ -76,7 +62,35 @@
 			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
 		</div>
 		<div class="col border border-primary task-label">
-			<h2>Service Extenstion Time</h2>
+			<h2>Power Cable Extenstion Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Water Piping Extenstion Time</h2>
+		</div>
+		<div class="col border border-primary">
+			<h2>Numbers</h2>
+		</div>
+
+	</div>
+
+	<div class="row">
+
+		<div class="col col-lg-1 no-gutter status">
+			<img src="assets/img/Status_black.png" class="status-light rounded-circle">
+		</div>
+		<div class="col border border-primary task-label">
+			<h2>Consumable Parts Supply Time</h2>
 		</div>
 		<div class="col border border-primary">
 			<h2>Numbers</h2>
